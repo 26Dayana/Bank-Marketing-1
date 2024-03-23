2341db03a72792390c596ac3cd4e8a96a2951ebb
Neelapu Varshitha <101424994+Varshitha-Neelapu@users.noreply.github.com>
2024-03-23 13:53:52 +0530

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+st.title('Bank Marketing Analysis and Modeling', anchor=None)
+
+st.write("""
+    - [Exploratory Data Analysis](https://github.com/Varshitha-Neelapu/Bank-Marketing/blob/main/Bank_Marketing_with_ML.ipynb)
+    - [Dataset](https://archive.ics.uci.edu/static/public/222/bank+marketing.zip)
+""")
